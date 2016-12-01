e08edd72be4aaddd7b7fc0e26f3c1e8c7951841f
Ryan Nair <ryan.nair@standardbank.co.za>
2016-12-01 14:19:14 +0200

    first push

---
+++ app/src/test/java/com/example/ryan/test/MainActivityTest.java
@@ -0,0 +1,12 @@
+package com.example.ryan.test;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MainActivityTest{
+
+    @Test
+    public void name() throws Exception {
+    }
+}
